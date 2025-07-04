9c07facf2951c0eb4922cb7d71f8ca60d4f06abf
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-04 11:27:58 -0400

    started js

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container")
+const bppForm = document.getElementById("bppForm")
+const postTitle = document.getElementById("postTitle")
+const postContent = document.getElementById("postContent")
+const errorMessage =  document.querySelectorAll(".errorMessage")
+
+const displayPost = []
