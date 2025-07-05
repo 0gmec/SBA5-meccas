3e671b9cb15756bac6da6886db3a6b72f87a9a1a
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-05 18:45:41 -0400

    added event listeners

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ const container = document.querySelector(".container");
 const bppForm = document.getElementById("bppForm");
 const postTitle = document.getElementById("postTitle");
 const postContent = document.getElementById("postContent");
-const postTitleError =  document.getElementById("postTtileError");
-const postContentError = document.getElementById("postContentErrror");
+const postTitleError =  document.getElementById("postTitleError");
+const postContentError = document.getElementById("postContentError");
 const displayPost = document.getElementById("displayPost");
+const editBtn = document.getElementById("editBtn");
+const deleteBtn = document.getElementById("deleteBtn")
 
 let post = []
 
-function display() {
+function displayTitle() {
     const input = document.getElementById("postTitle");
     localStorage.setItem("postTitle", input.value);
 
 }
-function display() {
+function displayContent() {
     const input = document.getElementById("postContent");
     localStorage.setItem("postContent", input.value);
 }
 
-function renderPost() {
+function showPost() {
     const displayPost = document.getElementById("displayPost");
     displayPost.innerHTML = "";
     
@@ -26,10 +28,31 @@ function renderPost() {
     postTitle.value = "";
 
     const postContent = document.getElementById("postContent");
-    postContent.innerHTML = "";
+    postContent.value = "";
+
+    const editBtn = document.getElementById("editBtn");
+    editBtn.value = "";
+
+    const deleteBtn = document.getElementById("deleteBtn");
+    deleteBtn.value = ""; 
+
+    showPost.appendChild(displayPost);
+showPost.appendChild(postTitle);
+showPost.appendChild(postContent);
+showPost.appendChild(editBtn);
+showPost.appendChild(deleteBtn);
 }
+
 
 
 bppForm.addEventListener("submit", function (event) {
     event.preventDefault();
+})
+
+editBtn.addEventListener("click", function(){
+
+})
+
+deleteBtn.addEventListener("click", function(){
+    
 })
