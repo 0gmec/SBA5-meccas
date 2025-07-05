8d649763fa988fc659557e01c8ba5e992fdd9887
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-05 15:03:20 -0400

    adding styling

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const bppForm = document.getElementById("bppForm")
 const postTitle = document.getElementById("postTitle")
 const postContent = document.getElementById("postContent")
 const errorMessage =  document.querySelectorAll(".errorMessage")
+const displayPost = document.getElementById("displayPost")
 
-const displayPost = []
+
